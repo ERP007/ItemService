c3d3871b2fe627172a66b7b5bfd1ebd477a48d7e
taehyung5271 <kth14200@gmail.com>
2026-06-02 11:55:38 +0900

    fix (erp-00): healthcontroller 위치 변경

--- src/main/java/com/fallguys/itemservice/HealthController.java
+++ src/main/java/com/fallguys/itemservice/HealthController.java
@@ -1,3 +1,5 @@
+package com.fallguys.itemservice;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
